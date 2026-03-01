5e13da7da1f8da34b14ec66d6875890cfaf536e5
Yatin07 <lucifer940oy@gmail.com>
2026-03-01 23:41:31 +0530

    login activity made and initial setup

--- app/src/main/java/com/example/hostelcomplaintapp/MainActivity.java
+++ app/src/main/java/com/example/hostelcomplaintapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hostelcomplaintapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btnStudent,btnStaff,btnWarden;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button btnStudent = findViewById(R.id.btnStudent);
+        Button btnStaff = findViewById(R.id.btnStaff);
+        Button btnWarden = findViewById(R.id.btnWarden);
+
+        btnStudent.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.putExtra("role", "student");
+            startActivity(intent);
+        });
+
+        btnStaff.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.putExtra("role", "staff");
+            startActivity(intent);
+        });
+
+        btnWarden.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            intent.putExtra("role", "warden");
+            startActivity(intent);
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
