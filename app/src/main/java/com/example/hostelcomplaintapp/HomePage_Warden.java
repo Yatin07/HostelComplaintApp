d978b16b349a7b879ee2872836e419e5c0cf9db3
Chinmay89910 <cgpatil2004@gmail.com>
2026-03-17 22:34:05 +0530

    designed login page

--- app/src/main/java/com/example/hostelcomplaintapp/HomePage_Warden.java
+++ app/src/main/java/com/example/hostelcomplaintapp/HomePage_Warden.java
@@ -1,6 +1,9 @@
 package com.example.hostelcomplaintapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomePage_Warden extends AppCompatActivity {
 
+    TextView tv1,tv2;
+    EditText et1,et2;
+    Button bt1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page_warden);
+
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+        bt1=findViewById(R.id.bt1);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
